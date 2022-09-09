e68b0437af8b936dded266ed9eb173089498ddd7
alexei-alekseev <alexei.aleks@gmail.com>
2022-09-09 18:20:27 +0300

    Добавил эмоджи и qa

--- js.js
+++ js.js
@@ -3,5 +3,5 @@ const button = document.querySelector('button');
 
 /* Тут на кнопку навешиваем обрабочик, который ждёт клика и тогда запустит логику */
 button.addEventListener('click', function() {
-	alert('Keep calm and believe in yourself')
+	alert('Keep calm and believe in yourself :)')
 })
